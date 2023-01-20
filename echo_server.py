76e0a5816229da718053a4bc8570da268ad68adb
Dave Goel <73494170+spiderassassin@users.noreply.github.com>
2023-01-20 01:08:48 -0700

    Update echo_server.py

--- echo_server.py
+++ echo_server.py
@@ -9,14 +9,7 @@ def handle_connection(conn, addr):
      with conn:
         print(f"Connected by {addr}")
         while True:
-            '''
-            recv(bytes) 
-            https://manpages.debian.org/bullseye/manpages-dev/recv.2.en.html
-
-            When a stream socket peer has performed an orderly shutdown, the return value will be 0 (the traditional "end-of-file" return).
-
-            In python 0 is falsey.
-            '''
+           
             data = conn.recv(BYTES_TO_READ)
             if not data:
                 break
@@ -28,10 +21,7 @@ def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST,PORT))
 
-        '''
-        setsockopt(level, option, value)
-        https://manpages.debian.org/bullseye/manpages-dev/setsockopt.2.en.html
-        '''
+        
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen()
         conn, addr = s.accept()
@@ -42,10 +32,7 @@ def start_threaded_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST,PORT))
 
-        '''
-        setsockopt(level, option, value)
-        https://manpages.debian.org/bullseye/manpages-dev/setsockopt.2.en.html
-        '''
+       
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen(2) # Allow backlog of up to 2 connections
         while True:
